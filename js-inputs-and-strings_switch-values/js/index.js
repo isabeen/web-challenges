2dbff795f6fecf2c86475254116626bc63c6ea7a
Sabeen Javaid <khannssabeen@gmail.com>
2024-05-14 10:08:33 +0200

    update inputs and strings challenges

--- js-inputs-and-strings_switch-values/js/index.js
+++ js-inputs-and-strings_switch-values/js/index.js
@@ -21,14 +21,16 @@ const switchButton = document.querySelector('[data-js="button-switch"]');
 
 uppercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toUpperCase();
+  secondInput.value = secondInput.value.toUpperCase();
 });
 
 lowercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toLowerCase();
+  secondInput.value = secondInput.value.toLowerCase();
 });
 
 switchButton.addEventListener("click", () => {
-  let newFirstInput = firstInput.value;
+  let initialFirstInput = firstInput.value;
   firstInput.value = secondInput.value;
-  secondInput.value = newFirstInput;
+  secondInput.value = initialFirstInput;
 });

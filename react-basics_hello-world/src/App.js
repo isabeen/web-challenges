a3deaf45fc729f0b6cbc3581f59495d31a5960ca
Sabeen Javaid <khannssabeen@gmail.com>
2024-05-27 15:36:49 +0200

    add reach basics Hello World challenge

---
+++ react-basics_hello-world/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <HelloWorld />
+    </div>
+  );
+}
+
+function HelloWorld() {
+  return "Say hello...";
+}

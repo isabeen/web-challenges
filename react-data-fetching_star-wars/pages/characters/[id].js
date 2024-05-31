3b99700ed7a2efc4096373f1f253252e40ae2535
Sabeen Javaid <khannssabeen@gmail.com>
2024-05-31 16:35:09 +0200

    add star wars challenge

---
+++ react-data-fetching_star-wars/pages/characters/[id].js
@@ -0,0 +1,33 @@
+import Card from "../../components/Card";
+import Layout from "../../components/Layout";
+import useSWR from "swr";
+import { useRouter } from "next/router";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+export default function Character() {
+  // const id = 1;
+  const router = useRouter();
+  const { id } = router.query;
+
+  const { data, error, isLoading } = useSWR(
+    `https://swapi.dev/api/people/${id}`,
+    fetcher
+  );
+
+  if (isLoading) return <div>loading...</div>;
+  if (error) return <div>failed to load</div>;
+
+  console.log(data);
+
+  return (
+    <Layout>
+      <Card
+        id={id}
+        name={data.name}
+        height={data.height}
+        eyeColor={data.eye_color}
+        birthYear={data.birth_year}
+      />
+    </Layout>
+  );
+}
